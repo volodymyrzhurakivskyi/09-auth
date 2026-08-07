494dd6b943eb344c1745a7fa01968c97e474ea50
Volodymyr <zhurakivskyivolodymyr@gmail.com>
2026-08-07 11:04:22 +0300

    fix-proxy-home-9

--- proxy.ts
+++ proxy.ts
@@ -5,7 +5,7 @@ import { checkSession } from '@/lib/api/serverApi';
 const privateRoutes = ['/profile', '/notes'];
 const publicRoutes = ['/sign-in', '/sign-up'];
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const isPrivateKey = privateRoutes.some((route) =>
