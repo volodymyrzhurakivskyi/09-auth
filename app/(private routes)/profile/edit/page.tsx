83ab180a9687d168c4a06d532ad104a110ab18af
Volodymyr <zhurakivskyivolodymyr@gmail.com>
2026-08-07 11:11:08 +0300

    fix-edit

--- app/(private routes)/profile/edit/page.tsx
+++ app/(private routes)/profile/edit/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { updateMe } from '@/lib/api/clientApi';
 import { useAuthStore } from '@/lib/store/authStore';
-import css from './EditProfile.module.css'; // Перевірте назву CSS файлу
+import css from './EditProfilePage.module.css';
 
 export default function EditProfilePage() {
   const router = useRouter();
